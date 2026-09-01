6be7e5f6a06ca11cb883d8ac36cd7ed78e7a2dd3
Zaxxced <142769904+zaxxced@users.noreply.github.com>
2026-09-01 09:22:04 +0700

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -5,7 +5,7 @@ export type Member = {
   bio: string
   avatar: string
   href: string
-  accent: 'blue' | 'sage' | 'peach' | 'lilac' | 'slate' |
+  accent: 'blue' | 'sage' | 'peach' | 'lilac' | 'slate' 
 }
 
 export const members: Member[] = [
